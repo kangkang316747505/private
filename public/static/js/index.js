33225de698f384cd04beefdbd12073e9dd1ce93b
niushaokang <316747505@qq.com>
2018-07-04 10:42:48 +0800

    修改弹出层问题

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,6 +1,7 @@
 $(function(){
    
     vailPhone();
+    var reveal = $(".mallmask").css({"padding": "0.3rem 0.5rem"});
     //判断手机号
     function vailPhone() {
         $('#signChack').on('click', function () {
@@ -8,16 +9,20 @@ $(function(){
         var myreg = /^1[34578]\d{9}$/;
             if (phone == '') {
                 $('.mallmask').html('手机号码不能为空');
+                reveal;
                 $(".mallmask").show().delay(3000).hide(300);
             }else if (phone.length != 11) {
+                reveal
                 $('.mallmask').html('手机号码未输入指定位数');
+                reveal;
                 $(".mallmask").show().delay(3000).hide(300);
             }else if (!myreg.test(phone)) {
                 $('.mallmask').html('手机号码不正确');
+                reveal;
                 $(".mallmask").show().delay(3000).hide(300);
             }
             else if (myreg.test(phone)) {
-                $(".mallmask").show();
+                reveal;
                 checkPhoneIsExist();
             }
         }
@@ -35,25 +40,31 @@ $(function(){
             success: function (result) {
                 var json = JSON.parse(result);
                 if (json.code == 200) {
-                    if(json.data.is_type == 3 ){
+                    if(json.data.is_type == '3' ){
                         $('.mallmask2').html('签到成功,请领取' + json.data.count+'个大礼包');
+                        reveal;
                         $(".mallmask").show();
-                    }else if (json.data.is_type == 2) {
+                    }else if (json.data.is_type == '2') {
                          $('.mallmask2').html('签到成功,请领取' + json.data.count + '个小礼包');
+                         reveal;
                         $(".mallmask").show();
-                    }else if (json.data.is_type == 1) {
+                    }else if (json.data.is_type == '1') {
                         $('.mallmask2').html('签到成功,请领取' + json.data.count + '张门票');
+                        reveal;
                         $(".mallmask").show();
                     }
                 } else if (json.code == 401) {
                 } else if (json.code == 402) {
                    $('.mallmask2').html('您已经签过到了哟~');
+                   reveal;
                    $(".mallmask").show().delay(3000).hide(300);
                 } else if (json.code == 403) {
                    $('.mallmask2').html('报名成功请领取一张门票哟~');
-                   $(".mallmask").show().delay(3000).hide(300);
+                   reveal;
+                   $(".mallmask").show();
                 } else if (json.code == 202) {
                     $('.mallmask2').html('您签到失败了哟~');
+                    reveal;
                     $(".mallmask").show().delay(3000).hide(300);
                 }   
             }
