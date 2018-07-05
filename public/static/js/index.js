1297ab0b3a906f3d080d254631dff5177bac57b0
niushaokang <316747505@qq.com>
2018-07-05 11:36:20 +0800

    头部遮盖

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,5 +1,20 @@
 $(function(){
-   
+    isIPhoneX();
+    function isIPhoneX() {
+        var u = navigator.userAgent;
+        var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+        if (isIOS) {
+            if (screen.height == 812 && screen.width == 375) {
+                $('.gather').css({
+                    "height":"21.65rem"
+                })
+            } else {
+                $('.gather').css({
+                    "height": "100%"
+                })
+            }
+        }
+    }
     vailPhone();
     var reveal = $(".mallmask").css({"padding": "0.3rem 0.5rem"});
     //判断手机号
